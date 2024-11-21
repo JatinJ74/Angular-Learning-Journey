5d5b2ea40228cd65875de3d5e249108178f4f165
Jatin Jaiswal <117575812+JatinJ74@users.noreply.github.com>
2024-11-21 20:14:13 +0530

    added components and images

--- Blogpost-Project/src/app/app.component.ts
+++ Blogpost-Project/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../components/header/header.component";
+import { FooterComponent } from "../components/footer/footer.component";
+import { HomeComponent } from "../components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'blogpost';
--- Blogpost-Project/src/app/app.routes.ts
+++ Blogpost-Project/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from '../components/blog/blog.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+   
+];
